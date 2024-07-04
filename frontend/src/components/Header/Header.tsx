82a24ff43ddf94f23b37ed5f64ae456a05a2a48b
Emir Jean Antonios <emir.antonios@gmail.com>
2024-07-04 23:04:22 +0300

    feature: svg turned into component

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import { useGetRequest } from "../../hooks/useGetRequest";
 import useDebounceCallback from "../../hooks/useDebounceCallback";
 import SearchBox from "./components/SearchBox/SearchBox";
 import { IPlaceApiResponse } from "../../interfaces/IPlaceApiResponse";
+import SearchIcon from "../../Icons/SearchIcon";
 
 const Header = () => {
   const [query, setQuery] = useState("");
@@ -56,22 +57,7 @@ const Header = () => {
           }}
           className="lg:w-[500px]"
           slots={{
-            rightEndAdornment: (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="white"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                />
-              </svg>
-            ),
+            rightEndAdornment: <SearchIcon />,
           }}
         >
           <Input.DropDown isOpen={isDropdownOpen}>
--- frontend/src/views/MapView/MapView.tsx
+++ frontend/src/views/MapView/MapView.tsx
@@ -1,10 +1,10 @@
-import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useLocationStore } from "../../stores/useLocationStore";
 import { useEffect } from "react";
 
 const MapView = () => {
   return (
-    <MapContainer  center={[51, -1]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={[40.5698389, 34.7269292]} zoom={13} scrollWheelZoom={true}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <MapComponent />
     </MapContainer>
@@ -18,7 +18,14 @@ const MapComponent: React.FC = () => {
     map.setView([lat, lon]);
   }, [lat, lon, map]);
 
-  return <Marker position={[lat, lon]} />;
+  return (
+    <Marker position={[lat, lon]}>
+      <Popup>
+        latitude: {lat} <br />
+        longitude: {lon}
+      </Popup>
+    </Marker>
+  );
 };
 
 export default MapView;
